79821be735db0fadded4c772d49a1576474ad98d
KONGO ARENA <arenakongo@gmail.com>
2026-08-10 10:26:15 -0300

    Delete api directory

--- api/config/database.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Copie para database.php e preencha
-return [
-    'host' => 'localhost',
-    'dbname' => 'kongo_arena',
-    'username' => 'usuario',
-    'password' => 'senha',
-    'charset' => 'utf8mb4'
-];
